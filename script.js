8671b88c2c7db2f8a57e8c6d8cb2d5ecb5087ea5
ClaudioEzequiel <guzmanclaudio.ezequiel@gmail.com>
2023-05-10 19:27:52 -0300

    modifique el formulario y agregue una Api para el envio del mismo

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-// Obtener el formulario y los campos
-const form = document.getElementById("contact-form");
-const fnameField = document.getElementById("fname");
-const numberField = document.getElementById("number");
-const countryField = document.getElementById("country");
-const messageField = document.getElementById("message");
+// Utilizamos la API Formspree para enviar el formulario por email y devolver un mensaje al usuario ,previo al envio verificamos que los campos esten completos
 
-// Validar el formulario cuando se envía
-form.addEventListener("submit", function(event) {
+const $form = document.querySelector("#contact-form");
+
+$form.addEventListener("submit", handleSubmit);
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  // Obtener el formulario y los campos
+  const nameField = document.getElementById("name");
+  const numberField = document.getElementById("number");
+  const countryField = document.getElementById("country");
+  const messageField = document.getElementById("message");
+
   // Verificar si los campos obligatorios se han completado
-  if (fnameField.value === "" ||numberField.value === "" || countryField.value === "" || messageField.value === "") {
-    event.preventDefault(); // Evitar que se envíe el formulario
+  if (
+    nameField.value === "" ||
+    numberField.value === "" ||
+    countryField.value === "" ||
+    messageField.value === ""
+  ) {
     alert("Por favor complete todos los campos.");
+    return; //Evitamos que se envie el formulario
   }
-});
 
+  const form = new FormData(this);
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      accept: "application/json",
+    },
+  });
+  if (response.ok) {
+    alert("Gracias por contactarte, te escribiremos pronto");
+    nameField.value = nameField.defaultValue;
+    numberField.value = numberField.defaultValue;
+    countryField.value = countryField.defaultValue;
+    messageField.value = messageField.defaultValue;
+  }
+}
