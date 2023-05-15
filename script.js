b53bb12d688575120ae196f7b57bdfeafe9ac755
ClaudioEzequiel <guzmanclaudio.ezequiel@gmail.com>
2023-05-15 18:15:25 -0300

    Realice envio de alertas por separado en cada campo y ademas agregue una alerta por validacion de ingreso de numero

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Utilizamos la API Formspree para enviar el formulario por email y devolver un mensaje al usuario ,previo al envio verificamos que los campos esten completos
+// // Utilizamos la API Formspree para enviar el formulario por email y devolver un mensaje al usuario ,previo al envio verificamos que los campos esten completos
 
 const $form = document.querySelector("#contact-form");
 
@@ -7,20 +7,28 @@ $form.addEventListener("submit", handleSubmit);
 async function handleSubmit(event) {
   event.preventDefault();
   // Obtener el formulario y los campos
-  const nameField = document.getElementById("name");
+  const nameField = document.getElementById("fname");
   const numberField = document.getElementById("number");
   const countryField = document.getElementById("country");
   const messageField = document.getElementById("message");
+  const numberRegex = /^[0-9]+$/;
 
   // Verificar si los campos obligatorios se han completado
-  if (
-    nameField.value === "" ||
-    numberField.value === "" ||
-    countryField.value === "" ||
-    messageField.value === ""
-  ) {
-    alert("Por favor complete todos los campos.");
-    return; //Evitamos que se envie el formulario
+  if (nameField.value === "") {
+    alert("Por favor complete el campo NOMBRE");
+    return;
+  } else if (numberField.value === "") {
+    alert("Por favor complete el campo NÚMERO");
+    return;
+  } else if (!numberRegex.test(numberField.value)) {
+    alert("Por favor ingrese un NÚMERO válido");
+    return;
+  } else if (countryField.value === "") {
+    alert("Por favor seleccione el BARRIO");
+    return;
+  } else if (messageField.value === "") {
+    alert("Por favor escriba el MENSAJE");
+    return;
   }
 
   const form = new FormData(this);
@@ -31,6 +39,7 @@ async function handleSubmit(event) {
       accept: "application/json",
     },
   });
+
   if (response.ok) {
     alert("Gracias por contactarte, te escribiremos pronto");
     nameField.value = nameField.defaultValue;
